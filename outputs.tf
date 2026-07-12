82642cd19f2f2205047c94395d40f97e653cb824
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,43 +1,38 @@
-output "iothub_endpoint_servicebus_queues" {
-  description = "All iothub_endpoint_servicebus_queue resources"
-  value       = azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues
-  sensitive   = true
-}
 output "iothub_endpoint_servicebus_queues_authentication_type" {
-  description = "List of authentication_type values across all iothub_endpoint_servicebus_queues"
-  value       = [for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : v.authentication_type]
+  description = "Map of authentication_type values across all iothub_endpoint_servicebus_queues, keyed the same as var.iothub_endpoint_servicebus_queues"
+  value       = { for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : k => v.authentication_type }
 }
 output "iothub_endpoint_servicebus_queues_connection_string" {
-  description = "List of connection_string values across all iothub_endpoint_servicebus_queues"
-  value       = [for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : v.connection_string]
+  description = "Map of connection_string values across all iothub_endpoint_servicebus_queues, keyed the same as var.iothub_endpoint_servicebus_queues"
+  value       = { for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : k => v.connection_string }
   sensitive   = true
 }
 output "iothub_endpoint_servicebus_queues_endpoint_uri" {
-  description = "List of endpoint_uri values across all iothub_endpoint_servicebus_queues"
-  value       = [for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : v.endpoint_uri]
+  description = "Map of endpoint_uri values across all iothub_endpoint_servicebus_queues, keyed the same as var.iothub_endpoint_servicebus_queues"
+  value       = { for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : k => v.endpoint_uri }
 }
 output "iothub_endpoint_servicebus_queues_entity_path" {
-  description = "List of entity_path values across all iothub_endpoint_servicebus_queues"
-  value       = [for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : v.entity_path]
+  description = "Map of entity_path values across all iothub_endpoint_servicebus_queues, keyed the same as var.iothub_endpoint_servicebus_queues"
+  value       = { for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : k => v.entity_path }
 }
 output "iothub_endpoint_servicebus_queues_identity_id" {
-  description = "List of identity_id values across all iothub_endpoint_servicebus_queues"
-  value       = [for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : v.identity_id]
+  description = "Map of identity_id values across all iothub_endpoint_servicebus_queues, keyed the same as var.iothub_endpoint_servicebus_queues"
+  value       = { for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : k => v.identity_id }
 }
 output "iothub_endpoint_servicebus_queues_iothub_id" {
-  description = "List of iothub_id values across all iothub_endpoint_servicebus_queues"
-  value       = [for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : v.iothub_id]
+  description = "Map of iothub_id values across all iothub_endpoint_servicebus_queues, keyed the same as var.iothub_endpoint_servicebus_queues"
+  value       = { for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : k => v.iothub_id }
 }
 output "iothub_endpoint_servicebus_queues_name" {
-  description = "List of name values across all iothub_endpoint_servicebus_queues"
-  value       = [for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : v.name]
+  description = "Map of name values across all iothub_endpoint_servicebus_queues, keyed the same as var.iothub_endpoint_servicebus_queues"
+  value       = { for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : k => v.name }
 }
 output "iothub_endpoint_servicebus_queues_resource_group_name" {
-  description = "List of resource_group_name values across all iothub_endpoint_servicebus_queues"
-  value       = [for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : v.resource_group_name]
+  description = "Map of resource_group_name values across all iothub_endpoint_servicebus_queues, keyed the same as var.iothub_endpoint_servicebus_queues"
+  value       = { for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : k => v.resource_group_name }
 }
 output "iothub_endpoint_servicebus_queues_subscription_id" {
-  description = "List of subscription_id values across all iothub_endpoint_servicebus_queues"
-  value       = [for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : v.subscription_id]
+  description = "Map of subscription_id values across all iothub_endpoint_servicebus_queues, keyed the same as var.iothub_endpoint_servicebus_queues"
+  value       = { for k, v in azurerm_iothub_endpoint_servicebus_queue.iothub_endpoint_servicebus_queues : k => v.subscription_id }
 }
 
